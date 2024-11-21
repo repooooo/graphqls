1f051953e582a8366760631e8a92e8beaad26cd2
moskvinspace <me@moskvin.space>
2024-11-21 18:52:05 +0200

    v0.0.6

--- go/resolvers/auth/resolver.go
+++ go/resolvers/auth/resolver.go
@@ -1,8 +1,18 @@
 package authresolver
 
+import (
+	"context"
+	authmodel "github.com/repooooo/graphqls/go/gen/auth/model"
+)
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
+
+type Handler interface {
+	Login(ctx context.Context, input authmodel.LoginRequest) (*authmodel.LoginResponse, error)
+	Logout(ctx context.Context, input authmodel.LogoutRequest) (*authmodel.LogoutResponse, error)
+}
 
 type Resolver struct {
 	handler Handler
